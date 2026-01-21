7a381de834cfb55786ee9defcd4c163e74115183
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-01-21 17:54:02 +0530

    modified src/model/User.java

---
+++ src/model/User.java
@@ -0,0 +1,12 @@
+@Entity
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+}
