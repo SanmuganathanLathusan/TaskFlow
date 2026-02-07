5dfae9c47f22613c1f039bc208ce115382fc6117
SanmuganathanLathusan <sanmuganathanlathusan@gmail.com>
2026-02-07 21:52:26 +0530

    modified  src/main/java/com/example/taskflow/model/User.java

--- src/main/java/com/example/taskflow/model/User.java
+++ src/main/java/com/example/taskflow/model/User.java
@@ -1,4 +1,4 @@
-package com.example.taskflow.model;
+package com.example.taskflow.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -7,7 +7,6 @@ import jakarta.persistence.GenerationType;
 
 @Entity
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,15 +15,5 @@ public class User {
     private String email;
     private String password;
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
-
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    // getters and setters
 }
